e23b8ac7e83ac0f881e25228e667c7c825c3b676
Vince Andrew Pimentel <contact@vincep.me>
2019-01-07 00:43:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    "My name is Vince."
+  end
+
+  get "/" do
+    "My hometown is San Fernando."
+  end
+
+  get "/" do
+    "My favorite song is Shelter."
+  end
 end
